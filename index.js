3664bee099b332b63f1e3fe738a9a1d9499b82ad
Slava Biziuykin <farmik222@yandex.ru>
2019-11-28 19:37:15 +0300

    end

--- index.js
+++ index.js
@@ -1,37 +1,82 @@
+// localStorage.clear();
 var list = document.querySelector('ul');
+var data = [];
+if(localStorage.getItem('timerArrayData')){
+    data = JSON.parse(localStorage.getItem('timerArrayData'));
+    console.log(data);
+}
+if(data){
+    for(var i = 0; i< data.length; i++){
+        data[i].time = dateParse(data[i].time);
+        unloadElement(data[i]);
+    }
+}
 list.addEventListener('click', function (ev) {
     if(ev.target.tagName === "LI") {
         ev.target.classList.toggle('checked');
     } else if(ev.target.tagName === "SPAN") {
         var div = ev.target.parentNode;
+        var target = ev.target.parentNode.classList[0];
+        var interval = data.find(item => target == item.element).intervalId;
+        clearInterval(interval);
+        data = data.filter(item => target != item.element);
+        localStorage.setItem('timerArrayData', JSON.stringify(data, null, 2));
+        console.log(data);
     div.remove();
     }
 }, false);
 
-
+function unloadElement(elementData){
+    var li = document.createElement('li');
+    li.classList.add(`item${elementData.index}`);
+    updateElement(li, elementData.text, elementData.time);
+    document.getElementById('list').appendChild(li);
+}
 
 function newElement() {
     var li = document.createElement('li');
+    li.classList.add(`item${data.length}`);
     var inputText = document.getElementById('text').value;
     var inputTime = document.getElementById('time').value;
-    li.innerHTML=inputText + " " + getTimer();
+    var timerValue = new Date(document.getElementById('time').value);
+    var timerData = {
+        intervalId: '',
+        index: data.length,
+        element: li.classList[0],
+        text: inputText,
+        time: inputTime
+    }
+    data.push(timerData);
+    localStorage.setItem('timerArrayData', JSON.stringify(data, null, 2));
+    updateElement(li, inputText, timerValue);
     if(inputText == "" || inputTime == "") {
         alert("Введите дело и время");
     } else {
         document.getElementById('list').appendChild(li);
     }
-    document.getElementById('text').value = "";
-    var span = document.createElement('SPAN');
-    var txt = document.createTextNode("X");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+   
+}
+
+function updateElement(element, inputText, timerValue){
+    var intervalId = setInterval(() => {
+        var timerElement = data.find(item => element.classList == item.element);
+        data[timerElement.index].intervalId = intervalId;
+        data[timerElement.index].time = getTimer(timerValue);
+        localStorage.setItem('timerArrayData', JSON.stringify(data, null, 2));
+        element.innerHTML=inputText + " " + getTimer(timerValue);
+        console.log(localStorage.getItem('timerArrayData'));
+        var span = document.createElement('SPAN');
+        var txt = document.createTextNode("X");
+        span.className = "close";
+        span.appendChild(txt);
+        element.appendChild(span);
+    }, 1000);
 }
 
 
-function getTimer(){
+function getTimer(newTime){
     var now = new Date();
-    var newTime = new Date(document.getElementById('time').value);
+    // var newTime = new Date(document.getElementById('time').value);
     var diff = Math.ceil((newTime-now) / 1000);
 
     var days = Math.floor(diff / (60 * 60 * 24));
@@ -44,16 +89,21 @@ function getTimer(){
     var diff = diff % 60;
 
     var seconds = diff;
-    console.log(`${days}d ${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}`);
+    // console.log(`${days}d ${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}`);
 
-    return `${days}d ${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}`
+    return `${days}:${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}`
+    // return `${days}:${(hours)}:${(minutes)}:${(seconds)}`
+
+
 }
 
-setInterval(getTimer,1000);
-setInterval(()=> {
-    console.log('lol');
-    getTimer();
-},1000);
+function dateParse(date){
+    var dateArray = date.split(':');
+    console.log(dateArray);
+    var date = new Date();
+    date.setSeconds(date.getSeconds()+ +dateArray[0] * 86400 + +dateArray[1] * 3600 + +dateArray[2] * 60 + +dateArray[3]);
+    return date;
+}
 
 function addZero(num){
     if(num <= 9){
@@ -62,56 +112,6 @@ function addZero(num){
     return num;
 }
 
-/*
-function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-        'total': t,
-        'days': days,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-    };
+function saveElements(){
+    console.log('lul');
 }
-
-function initializeClock(id, endtime) {
-    var now = new Date();
-    var newTime = new Date(document.getElementById('time').value);
-    var diff = Math.ceil((newTime-now) / 1000);
-
-    var days = Math.floor(diff / (60 * 60 * 24));
-    var diff = diff % (60 * 60 * 24);
-
-    var hours = Math.floor(diff / (60 * 60));
-    var diff = diff % (60 * 60);
-
-    var minutes = Math.floor(diff / 60);
-    var diff = diff % 60;
-
-    var seconds = diff;
-    
-    function updateClock() {
-        var t = getTimeRemaining(endtime);
-
-        days.innerHTML = t.days;
-        hours.innerHTML = ('0' + t.hours).slice(-2);
-        minutes.innerHTML = ('0' + t.minutes).slice(-2);
-        seconds.innerHTML = ('0' + t.seconds).slice(-2);
-
-        if (t.total <= 0) {
-        clearInterval(timeinterval);
-        }
-    }
-    
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
-    }
-
-    var deadline=document.getElementById('time').value;; //for Ukraine
-    var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
-    initializeClock('list', deadline);
-*/
